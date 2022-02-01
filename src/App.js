fc3441538ed5e4466125997e777cf12d19c40fee
Olga <olgadorogko@gmail.com>
2022-02-01 00:11:49 -0500

    Add "new expense" form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import ExpenseItemsList from "./components/Expenses/ExpenseItemsList/ExpenseItemsList";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Layout from "./components/UI/Layout/Layout";
 
 const App = () => {
   return (
     <div className="app">
       <Layout>
+        <NewExpense/>
         <ExpenseItemsList />
       </Layout>
     </div>
--- src/components/NewExpense/ExpenseForm/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.js
@@ -1,21 +1,72 @@
+import { useState } from "react";
 import "./ExpenseForm.scss";
 
 const ExpenseForm = () => {
+  const [state, setState] = useState({
+    title: "",
+    amount: "",
+    date: "",
+  });
+
+  const changeHandler = (event) => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: state.title,
+      amount: state.amount,
+      date: state.date,
+    };
+    console.log(expenseData);
+    setState({
+      title: "",
+      amount: "",
+      date: "",
+    });
+  };
+
   return (
     <div className="expense-form">
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <div className="controls">
           <div className="control">
             <label>Title</label>
-            <input type="text" />
+            <input
+              type="text"
+              name="title"
+              value={state.title}
+              onChange={changeHandler}
+            />
           </div>
           <div className="control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              name="amount"
+              value={state.amount}
+              onChange={changeHandler}
+            />
           </div>
           <div className="control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2022-12-31" />
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              name="date"
+              value={state.date}
+              onChange={changeHandler}
+            />
           </div>
         </div>
         <div className="actions">
